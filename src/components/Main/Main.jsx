02893ef322306436e78d77393414f891afa08ad5
Ibragim <oloven1337@gmail.com>
2021-08-18 16:29:55 +0300

    changed realization search panel and adding css

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,23 +5,18 @@ import {getApiCharactersOfName} from "../../utils/network";
 import Character from "./Character";
 
 const Main = () => {
-
-    const [query, setQuery] = React.useState('')
-    const [state, setState] = React.useState('')
+    const [requestText, setRequestText] = React.useState('')
     const [imgUrl, setImgUrl] = React.useState(hulk)
     const [character, setCharacter] = React.useState([])
     const [notFound, setNotFound] = React.useState(false)
 
-    //effect for search input delay
-    React.useEffect(() => {
-        const timeOutId = setTimeout(() => setState(query), 300)
-        return () => clearTimeout(timeOutId)
-    }, [query])
-    //
+    const handleOnChange = (event) => setRequestText(event.target.value)
 
-    React.useEffect(() => {
-        if (state !== '') {
-            getApiCharactersOfName(state)
+    const handleOnClick = event => {
+        event.preventDefault()
+
+        if (requestText !== '') {
+            getApiCharactersOfName(requestText)
                 .then((res) => {
                     if (!res) {
                         setNotFound(true)
@@ -34,25 +29,24 @@ const Main = () => {
                             {title: 'Name', data: res.data.results[0].name},
                             {title: 'Description', data: res.data.results[0].description}
                         ])
-                        console.log(res.data.results[0])
                     }
                 })
         }
-    }, [state])
-
-    console.log(character)
+    }
 
     return (
-        <div className={styles.container}>
-            <h1 className={styles.header}>Find character</h1>
-            <div className={styles.search_wrapper}>
-                <input className={styles.input} onChange={event => setQuery(event.target.value)}
-                       value={query} type="text"/>
+        <>
+            <form className={styles.container}>
+                <h1 className={styles.header}>Find character</h1>
+                <div className={styles.search_wrapper}>
+                    <input className={styles.input} onChange={handleOnChange}
+                           value={requestText} type="text"/>
+                    <button className={styles.button_search} onClick={handleOnClick}>Search</button>
+                </div>
                 <p className={styles.subheader}>for example - Hulk</p>
-            </div>
-
-            <Character character={character} imgUrl={imgUrl} notFound={notFound}/>
-        </div>
+                <Character character={character} imgUrl={imgUrl} notFound={notFound}/>
+            </form>
+        </>
     )
 }
 

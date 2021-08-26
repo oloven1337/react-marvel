c3b205d1f4730cdddf5e027f3f014d7b671dcdf6
Ibragim <oloven1337@gmail.com>
2021-08-26 21:31:39 +0300

    fix link to buy comics

--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -6,7 +6,7 @@ import styles from './Modal.module.css'
 const Modal = ({active, setModal, data}) => {
     console.log(data)
     const {description, images, urls, title} = data
-
+    console.log(urls[0])
     return (
         <div className={active ? cn(styles.modal, styles.active) : styles.modal} onClick={() => setModal(false)}>
             <div className={styles.modal_content} onClick={event => event.stopPropagation()}>
@@ -18,7 +18,7 @@ const Modal = ({active, setModal, data}) => {
                     <p className={styles.description}>
                         {description ? description : 'There is no data about the comic :('}
                     </p>
-                    <a className={styles.buy_link} href={urls}>BUY</a>
+                    <a className={styles.buy_link} href={urls[0].url} target="_blank">BUY</a>
                 </div>
                 <img onClick={() => setModal(false)} className={styles.close_modal} src={close} alt="close"/>
             </div>
